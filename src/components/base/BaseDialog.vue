4503b59cd4cd8fe73059883b2b895ab1ab05a83d
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-11-11 00:34:34 +0330

    Create BaseDialog.vue

---
+++ src/components/base/BaseDialog.vue
@@ -0,0 +1,9 @@
+<template>
+  <img src="" alt="" />
+  <h2>Thanks for subscribing!</h2>
+  <p>
+    A confirmation email has been sent to ash@loremcompany.com. Please open it
+    and click the button inside to confirm your subscription.
+  </p>
+  <button>Dismiss message</button>
+</template>
