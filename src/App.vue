2777fa1ea7987f3df24acd904504f76435e051ea
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-11-11 00:34:30 +0330

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,16 @@
-<template></template>
+<template>
+  <h1>Stay updated!</h1>
+  <p>Join 60,000+ product managers receiving monthly updates on:</p>
+  <ul>
+    <li>Product discovery and building what matters</li>
+    <li>Measuring to ensure updates are a success</li>
+    <li>And much more!</li>
+  </ul>
+  <form>
+    <label for="email">Email address</label>
+    <input type="email" id="email" placeholder="email@company.com" />
+    <button>Subscribe to monthly newsletter</button>
+  </form>
+</template>
 
 <script></script>
