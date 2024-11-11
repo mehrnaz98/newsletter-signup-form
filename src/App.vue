e83fc486fa1611268ae3db8afbc95467bcf455ce
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-11-11 21:33:19 +0330

    Update width

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="flex min-h-screen justify-center items-center bg-[#36384D]">
-    <div class="container bg-white rounded-3xl max-w-[50rem] m-5">
+    <div class="container bg-white rounded-3xl w-auto sm:max-w-[50rem] m-5">
       <picture class="responsive-image">
         <source
           media="(min-width: 768px)"
