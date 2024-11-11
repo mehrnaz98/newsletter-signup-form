b8d01c084792a51c21d5415d51d55bc4f12413a2
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-11-11 21:28:46 +0330

    Set max width and apply margin

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="flex min-h-screen justify-center items-center bg-[#36384D]">
-    <div class="container bg-white rounded-3xl">
+    <div class="container bg-white rounded-3xl max-w-[50rem] m-5">
       <picture class="responsive-image">
         <source
           media="(min-width: 768px)"
