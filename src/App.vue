c8685886ca96938770665690754ae6bc5617d80b
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-11-11 10:26:06 +0330

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <template>
-  <h1>Stay updated!</h1>
-  <p>Join 60,000+ product managers receiving monthly updates on:</p>
-  <ul>
-    <li>Product discovery and building what matters</li>
-    <li>Measuring to ensure updates are a success</li>
-    <li>And much more!</li>
-  </ul>
-  <form>
-    <label for="email">Email address</label>
-    <input type="email" id="email" placeholder="email@company.com" />
-    <button>Subscribe to monthly newsletter</button>
-  </form>
+  <main>
+    <h1 class="text-[3rem]">Stay updated!</h1>
+    <p class="text-[1rem]">
+      Join 60,000+ product managers receiving monthly updates on:
+    </p>
+    <ul>
+      <li>
+        <img src="./assets/images/icon-list.svg" alt="" /> Product discovery and
+        building what matters
+      </li>
+      <li>Measuring to ensure updates are a success</li>
+      <li>And much more!</li>
+    </ul>
+    <form>
+      <label for="email">Email address</label>
+      <input type="email" id="email" placeholder="email@company.com" />
+      <button>Subscribe to monthly newsletter</button>
+    </form>
+  </main>
 </template>
 
 <script></script>
