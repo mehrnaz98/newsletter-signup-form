1e47b9ba14af8c5afbd483b8745fcd81b094efa5
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-11-17 21:53:05 +0330

    Create TheForm.vue

---
+++ src/components/TheForm.vue
@@ -0,0 +1,16 @@
+<template>
+  <form class="flex flex-col">
+    <label for="email" class="roboto-bold text-[0.7rem] mt-7"
+      >Email address</label
+    >
+    <input
+      type="email"
+      id="email"
+      class="mt-2 border border-[#B3B2B7] text-[0.8rem] p-3 rounded-md"
+      placeholder="email@company.com"
+    />
+    <button class="bg-[#232742] text-white mt-4 text-[0.8rem] p-3 rounded-md">
+      Subscribe to monthly newsletter
+    </button>
+  </form>
+</template>
