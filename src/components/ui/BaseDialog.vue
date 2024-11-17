3b1cd6cb29d01bfe46fe6543b59f040a9636e14f
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-11-18 00:07:20 +0330

    Set display flex col for the dialog, set max width and padding

--- src/components/ui/BaseDialog.vue
+++ src/components/ui/BaseDialog.vue
@@ -1,9 +1,11 @@
 <template>
-  <img src="../../assets/images/icon-success.svg" alt="" />
-  <h2>Thanks for subscribing!</h2>
-  <p>
-    A confirmation email has been sent to ash@loremcompany.com. Please open it
-    and click the button inside to confirm your subscription.
-  </p>
-  <button>Dismiss message</button>
+  <section class="flex flex-col justify-start items-start p-10 max-w-[35rem]">
+    <img src="../../assets/images/icon-success.svg" alt="" class="h-16" />
+    <h2>Thanks for subscribing!</h2>
+    <p>
+      A confirmation email has been sent to ash@loremcompany.com. Please open it
+      and click the button inside to confirm your subscription.
+    </p>
+    <button>Dismiss message</button>
+  </section>
 </template>
