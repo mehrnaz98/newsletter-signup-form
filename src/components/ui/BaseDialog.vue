fe9154ad51ecf00b1457e8cdd63fefa7554eb086
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-11-18 00:16:19 +0330

    Set roboto bold font, set font size and update line height

--- src/components/ui/BaseDialog.vue
+++ src/components/ui/BaseDialog.vue
@@ -3,8 +3,10 @@
     class="flex flex-col justify-start items-start p-14 max-w-[28rem] space-y-5 text-[#222742]"
   >
     <img src="../../assets/images/icon-success.svg" alt="" class="h-16" />
-    <h2>Thanks for subscribing!</h2>
-    <p>
+    <h2 class="roboto-bold text-[3rem] leading-none">
+      Thanks for subscribing!
+    </h2>
+    <p class="text-[0.9rem]">
       A confirmation email has been sent to ash@loremcompany.com. Please open it
       and click the button inside to confirm your subscription.
     </p>
