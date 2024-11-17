57084a32d6054ef9a300197c42f94c26bea83027
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-11-17 21:59:01 +0330

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,7 @@
   <main
     class="flex min-h-screen justify-center items-center bg-[#36384D] roboto-regular"
   >
-    <div class="container bg-white rounded-3xl w-auto sm:max-w-[50rem] m-5">
-      <picture class="responsive-image">
-        <source
-          media="(min-width: 768px)"
-          srcset="./assets/images/illustration-sign-up-desktop.svg"
-        />
-        <img src="./assets/images/illustration-sign-up-mobile.svg" alt="" />
-      </picture>
-      <FormSection />
-    </div>
+    <FormSection />
   </main>
 </template>
 
