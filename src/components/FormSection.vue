ecc72a32568a5721eb4d74bdc360326a62465e1a
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-11-17 21:49:38 +0330

    Create FormSection.vue

---
+++ src/components/FormSection.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="form-section space-y-3">
+    <h1 class="text-[2.8rem] roboto-bold">Stay updated!</h1>
+    <p class="text-[0.9rem]">
+      Join 60,000+ product managers receiving monthly updates on:
+    </p>
+    <ul class="custom-list text-[0.9rem]">
+      <li>Product discovery and building what matters</li>
+      <li>Measuring to ensure updates are a success</li>
+      <li>And much more!</li>
+    </ul>
+    <form class="flex flex-col">
+      <label for="email" class="roboto-bold text-[0.7rem] mt-7"
+        >Email address</label
+      >
+      <input
+        type="email"
+        id="email"
+        class="mt-2 border border-[#B3B2B7] text-[0.8rem] p-3 rounded-md"
+        placeholder="email@company.com"
+      />
+      <button class="bg-[#232742] text-white mt-4 text-[0.8rem] p-3 rounded-md">
+        Subscribe to monthly newsletter
+      </button>
+    </form>
+  </div>
+</template>
