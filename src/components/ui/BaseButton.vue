692760666482661dd440b9c91397a19e355646db
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-11-20 23:42:44 +0330

    Give button width of 100 percent

--- src/components/ui/BaseButton.vue
+++ src/components/ui/BaseButton.vue
@@ -1,7 +1,7 @@
 <template>
   <button
     :type="type"
-    class="bg-[#232742] text-white mt-4 text-[0.8rem] p-3 rounded-md"
+    class="bg-[#232742] w-[100%] text-white mt-4 text-[0.8rem] p-3 rounded-md"
   >
     <slot />
   </button>
