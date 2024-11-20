a0e6cbd4e59b15dd8dc21c4be25649e2117dc489
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-11-20 23:52:27 +0330

    Add shadow

--- src/components/ui/BaseButton.vue
+++ src/components/ui/BaseButton.vue
@@ -1,7 +1,7 @@
 <template>
   <button
     :type="type"
-    class="bg-[#232742] w-[100%] text-white mt-4 text-[0.8rem] p-3 rounded-md hover:bg-[#FF6740] transition-colors ease-in-out duration-300"
+    class="bg-[#232742] w-[100%] text-white mt-4 text-[0.8rem] p-3 rounded-md hover:bg-[#FF6740] hover:shadow-2xl hover:shadow-[#f98f75] transition-all ease-in-out duration-300"
   >
     <slot />
   </button>
