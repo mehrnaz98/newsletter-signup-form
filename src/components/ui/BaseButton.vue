d88e73569619865b29f3788193aae7090eedf640
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-11-20 23:47:48 +0330

    Add hover effect for the base button

--- src/components/ui/BaseButton.vue
+++ src/components/ui/BaseButton.vue
@@ -1,7 +1,7 @@
 <template>
   <button
     :type="type"
-    class="bg-[#232742] w-[100%] text-white mt-4 text-[0.8rem] p-3 rounded-md"
+    class="bg-[#232742] w-[100%] text-white mt-4 text-[0.8rem] p-3 rounded-md hover:bg-[#FF6740] transition-colors ease-in-out duration-300"
   >
     <slot />
   </button>
