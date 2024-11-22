a8553a7a69c333a7de6dac95bbc62dd15f71be8b
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-11-23 01:21:51 +0330

    Provide the email

--- src/components/FormSection.vue
+++ src/components/FormSection.vue
@@ -36,6 +36,9 @@ const isDialogVisible = ref(false);
 function setEmail(emailAddress) {
   email.value = emailAddress;
 }
+
+provide("email", email);
+
 function showDialog() {
   isDialogVisible.value = true;
 }
