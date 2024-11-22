19ec448d65083c64799d44996a3baf864aa1a60a
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-11-23 01:22:37 +0330

    Display the email

--- src/components/ui/BaseDialog.vue
+++ src/components/ui/BaseDialog.vue
@@ -7,8 +7,9 @@
       Thanks for subscribing!
     </h2>
     <p class="text-[0.9rem] pb-4">
-      A confirmation email has been sent to ash@loremcompany.com. Please open it
-      and click the button inside to confirm your subscription.
+      A confirmation email has been sent to <strong>{{ email }}</strong
+      >. Please open it and click the button inside to confirm your
+      subscription.
     </p>
     <base-button @click="dismissDialog">Dismiss message</base-button>
   </section>
