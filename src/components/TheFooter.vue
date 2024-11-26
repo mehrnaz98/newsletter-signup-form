2edaaf87fd010b5559711a17ba23e648772287ef
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-11-27 00:01:45 +0330

    Update TheFooter.vue

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer>
+    <div
+      class="sm:text-xs manrope-medium text-[0.7rem] text-center bg-[#36384D] text-[#4C4E61] pb-3"
+    >
+      Challenge by
+      <a
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank"
+        class="text-[#337261] hover:font-bold"
+        >Frontend Mentor</a
+      >. Coded by
+      <a
+        href="https://github.com/mehrnaz98/"
+        target="_blank"
+        class="text-[#337261] hover:font-bold"
+        >Mehrnaz Eftekhari</a
+      >.
+    </div>
+  </footer>
+</template>
